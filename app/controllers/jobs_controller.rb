c8e14c481ac42fc962e4033bcf09e3464f4bf538
hancehardy <hthardy@gmail.com>
2023-08-15 19:34:28 -0700

    index,new and show pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,29 @@
 class JobsController < ApplicationController
 
     def index
+       @jobs = Job.all.order('created_at ASC') 
     end
-    
+
+    def new
+    end
+
+    def create
+        @job = Job.new(job_params)
+        @job.save
+
+        redirect_to @job
+    end
+
+    def show
+        @job =Job.find(params[:id])
+    end
+
+    private
+
+        def job_params
+            params.require(:job).permit(:name, :contract_amount, :contract_date, :status)
+        end
+
+
+
 end
